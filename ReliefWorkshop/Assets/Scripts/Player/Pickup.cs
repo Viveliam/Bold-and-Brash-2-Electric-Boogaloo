92e3009288a686fe33d0052b811cc1dc1cedcbf4
cdabrt <carlobaretta@hotmail.com>
2023-09-28 11:33:00 +0200

    Winscene

--- ReliefWorkshop/Assets/Scripts/Player/Pickup.cs
+++ ReliefWorkshop/Assets/Scripts/Player/Pickup.cs
@@ -1,10 +1,12 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Player
 {
     public class Pickup : MonoBehaviour
     {
+        [SerializeField] int amountToWin = 3;
         public int amountOfPictures = 0;
         public AudioSource audioSource;
         public AudioClip audioClip;
@@ -17,6 +19,11 @@ namespace Player
                 amountOfPictures++;
                 print(amountOfPictures.ToString());
                 Destroy(other.gameObject);
+            }
+
+            if (amountOfPictures >= amountToWin)
+            {
+                SceneManager.LoadScene("Win");
             }
         }
     }
